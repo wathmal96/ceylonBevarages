d1583287f8c06a4abb37838f7e9b30e6e73e2091
wathmal96 <ranaweera1125vinura@gmail.com>
2024-03-04 15:13:46 +0530

    first commit

---
+++ src/SalesMan.java
@@ -0,0 +1,9 @@
+public class SalesMan {
+    private String name;
+    private Product[] products;
+
+    public SalesMan(String name, Product[] products) {
+        this.name = name;
+        this.products = products;
+    }
+}
